09f6b808e858099aaa2f906602dc647844b01f1f
Mark Allen <mark.a.allen@gmail.com>
2022-12-22 14:28:43 -0500

    Add tags to topic center calls

--- src/main/java/com/cobaltplatform/api/model/api/response/TopicCenterApiResponse.java
+++ src/main/java/com/cobaltplatform/api/model/api/response/TopicCenterApiResponse.java
@@ -19,9 +19,13 @@
 
 package com.cobaltplatform.api.model.api.response;
 
+import com.cobaltplatform.api.context.CurrentContext;
+import com.cobaltplatform.api.model.api.response.TagApiResponse.TagApiResponseFactory;
 import com.cobaltplatform.api.model.api.response.TopicCenterRowApiResponse.TopicCenterRowApiResponseFactory;
+import com.cobaltplatform.api.model.db.Tag;
 import com.cobaltplatform.api.model.db.TopicCenter;
 import com.cobaltplatform.api.model.service.TopicCenterRowDetail;
+import com.cobaltplatform.api.service.TagService;
 import com.cobaltplatform.api.service.TopicCenterService;
 import com.cobaltplatform.api.util.Formatter;
 import com.google.inject.assistedinject.Assisted;
@@ -31,7 +35,10 @@ import com.lokalized.Strings;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
+import javax.inject.Provider;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -53,6 +60,8 @@ public class TopicCenterApiResponse {
 	private final String urlName;
 	@Nonnull
 	private final List<TopicCenterRowApiResponse> topicCenterRows;
+	@Nonnull
+	private final Map<String, TagApiResponse> tagsByTagId;
 
 	// Note: requires FactoryModuleBuilder entry in AppModule
 	@ThreadSafe
@@ -63,12 +72,18 @@ public class TopicCenterApiResponse {
 
 	@AssistedInject
 	public TopicCenterApiResponse(@Nonnull TopicCenterService topicCenterService,
+																@Nonnull TagService tagService,
 																@Nonnull TopicCenterRowApiResponseFactory topicCenterRowApiResponseFactory,
+																@Nonnull TagApiResponseFactory tagApiResponseFactory,
+																@Nonnull Provider<CurrentContext> currentContextProvider,
 																@Nonnull Formatter formatter,
 																@Nonnull Strings strings,
 																@Assisted @Nonnull TopicCenter topicCenter) {
 		requireNonNull(topicCenterService);
+		requireNonNull(tagService);
 		requireNonNull(topicCenterRowApiResponseFactory);
+		requireNonNull(tagApiResponseFactory);
+		requireNonNull(currentContextProvider);
 		requireNonNull(formatter);
 		requireNonNull(strings);
 		requireNonNull(topicCenter);
@@ -83,6 +98,13 @@ public class TopicCenterApiResponse {
 		this.topicCenterRows = topicCenterRows.stream()
 				.map(topicCenterRowDetail -> topicCenterRowApiResponseFactory.create(topicCenterRowDetail))
 				.collect(Collectors.toList());
+
+		Map<String, TagApiResponse> tagsByTagId = new HashMap<>();
+
+		for (Tag tag : tagService.findTagsByInstitutionId(currentContextProvider.get().getInstitutionId()))
+			tagsByTagId.put(tag.getTagId(), tagApiResponseFactory.create(tag));
+
+		this.tagsByTagId = tagsByTagId;
 	}
 
 	@Nonnull
@@ -108,5 +130,10 @@ public class TopicCenterApiResponse {
 	@Nonnull
 	public List<TopicCenterRowApiResponse> getTopicCenterRows() {
 		return this.topicCenterRows;
+	}
+
+	@Nonnull
+	public Map<String, TagApiResponse> getTagsByTagId() {
+		return this.tagsByTagId;
 	}
 }
