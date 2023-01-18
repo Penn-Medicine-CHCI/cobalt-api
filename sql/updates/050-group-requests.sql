8bf9e0a82a12c4e09497bf16f44e03dfdd33eba8
Mark Allen <mark.a.allen@gmail.com>
2023-01-18 12:29:15 -0500

    Starting on group requests

---
+++ sql/updates/050-group-requests.sql
@@ -0,0 +1,52 @@
+BEGIN;
+SELECT _v.register_patch('050-group-requests', NULL, NULL);
+
+CREATE TABLE group_topic (
+	group_topic_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	institution_id TEXT NOT NULL REFERENCES institution,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	display_order INTEGER NOT NULL DEFAULT 1,
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON group_topic FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+CREATE TABLE group_request (
+	group_request_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	requestor_account_id UUID NOT NULL REFERENCES account(account_id),
+	requestor_name TEXT NOT NULL,
+	requestor_email_address TEXT NOT NULL,
+	preferred_date_description TEXT,
+	preferred_time_description TEXT,
+	additional_description TEXT,
+	minimum_attendee_count INTEGER NOT NULL,
+	maximum_attendee_count INTEGER,
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON group_request FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+CREATE TABLE group_request_topic (
+	group_request_id UUID NOT NULL REFERENCES group_request,
+	group_topic_id UUID NOT NULL REFERENCES group_topic,
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (group_request_id, group_topic_id)
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON group_request_topic FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+CREATE TABLE group_request_institution_contact (
+	institution_id TEXT NOT NULL REFERENCES institution,
+	email_address TEXT NOT NULL,
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (institution_id, email_address)
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON group_request_topic FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+COMMIT;
---
+++ src/main/java/com/cobaltplatform/api/model/db/GroupRequest.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2021 The University of Pennsylvania and Penn Medicine
+ *
+ * Originally created at the University of Pennsylvania and Penn Medicine by:
+ * Dr. David Asch; Dr. Lisa Bellini; Dr. Cecilia Livesey; Kelley Kugler; and Dr. Matthew Press.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cobaltplatform.api.model.db;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * @author Transmogrify, LLC.
+ */
+@NotThreadSafe
+public class GroupRequest {
+	@Nullable
+	private UUID groupRequestId;
+	@Nullable
+	private UUID requestorAccountId;
+	@Nullable
+	private String requestorName;
+	@Nullable
+	private String requestorEmailAddress;
+	@Nullable
+	private String preferredDateDescription;
+	@Nullable
+	private String preferredTimeDescription;
+	@Nullable
+	private String additionalDescription;
+	@Nullable
+	private Integer minimumAttendeeCount;
+	@Nullable
+	private Integer maximumAttendeeCount;
+	@Nullable
+	private Instant created;
+	@Nullable
+	private Instant lastUpdated;
+
+	@Nullable
+	public UUID getGroupRequestId() {
+		return this.groupRequestId;
+	}
+
+	public void setGroupRequestId(@Nullable UUID groupRequestId) {
+		this.groupRequestId = groupRequestId;
+	}
+
+	@Nullable
+	public UUID getRequestorAccountId() {
+		return this.requestorAccountId;
+	}
+
+	public void setRequestorAccountId(@Nullable UUID requestorAccountId) {
+		this.requestorAccountId = requestorAccountId;
+	}
+
+	@Nullable
+	public String getRequestorName() {
+		return this.requestorName;
+	}
+
+	public void setRequestorName(@Nullable String requestorName) {
+		this.requestorName = requestorName;
+	}
+
+	@Nullable
+	public String getRequestorEmailAddress() {
+		return this.requestorEmailAddress;
+	}
+
+	public void setRequestorEmailAddress(@Nullable String requestorEmailAddress) {
+		this.requestorEmailAddress = requestorEmailAddress;
+	}
+
+	@Nullable
+	public String getPreferredDateDescription() {
+		return this.preferredDateDescription;
+	}
+
+	public void setPreferredDateDescription(@Nullable String preferredDateDescription) {
+		this.preferredDateDescription = preferredDateDescription;
+	}
+
+	@Nullable
+	public String getPreferredTimeDescription() {
+		return this.preferredTimeDescription;
+	}
+
+	public void setPreferredTimeDescription(@Nullable String preferredTimeDescription) {
+		this.preferredTimeDescription = preferredTimeDescription;
+	}
+
+	@Nullable
+	public String getAdditionalDescription() {
+		return this.additionalDescription;
+	}
+
+	public void setAdditionalDescription(@Nullable String additionalDescription) {
+		this.additionalDescription = additionalDescription;
+	}
+
+	@Nullable
+	public Integer getMinimumAttendeeCount() {
+		return this.minimumAttendeeCount;
+	}
+
+	public void setMinimumAttendeeCount(@Nullable Integer minimumAttendeeCount) {
+		this.minimumAttendeeCount = minimumAttendeeCount;
+	}
+
+	@Nullable
+	public Integer getMaximumAttendeeCount() {
+		return this.maximumAttendeeCount;
+	}
+
+	public void setMaximumAttendeeCount(@Nullable Integer maximumAttendeeCount) {
+		this.maximumAttendeeCount = maximumAttendeeCount;
+	}
+
+	@Nullable
+	public Instant getCreated() {
+		return this.created;
+	}
+
+	public void setCreated(@Nullable Instant created) {
+		this.created = created;
+	}
+
+	@Nullable
+	public Instant getLastUpdated() {
+		return this.lastUpdated;
+	}
+
+	public void setLastUpdated(@Nullable Instant lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+}
