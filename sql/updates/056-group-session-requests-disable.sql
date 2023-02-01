737435412f3153b524d374e85c66981f74493b8e
Mark Allen <mark.a.allen@gmail.com>
2023-02-01 11:11:05 -0500

    Permit group session requests for COBALT institution

--- sql/updates/056-group-session-requests-disable.sql
+++ sql/updates/056-group-session-requests-disable.sql
@@ -3,4 +3,7 @@ SELECT _v.register_patch('056-group-session-requests-disable', NULL, NULL);
 
 ALTER TABLE institution ADD COLUMN group_session_requests_enabled BOOLEAN NOT NULL DEFAULT FALSE;
 
+-- Turn on for just this institution
+UPDATE institution SET group_session_requests_enabled = TRUE WHERE institution_id='COBALT';
+
 COMMIT;
