8e4ab0c1742f3080cc7d6ad0a053ef4aa036794b
Mark Allen <mark.a.allen@gmail.com>
2023-03-18 13:18:32 -0400

    Adjusting how we model IC outreach results/types/statuses

--- sql/updates/067-patient-order-additions.sql
+++ sql/updates/067-patient-order-additions.sql
@@ -15,55 +15,74 @@ CREATE TABLE patient_order_outreach_result_status (
   description VARCHAR NOT NULL
 );
 
+INSERT INTO patient_order_outreach_result_status VALUES ('UNKNOWN', 'Unknown');
 INSERT INTO patient_order_outreach_result_status VALUES ('CONNECTED', 'Connected');
 INSERT INTO patient_order_outreach_result_status VALUES ('NOT_CONNECTED', 'Not Connected');
-INSERT INTO patient_order_outreach_result_status VALUES ('UNKNOWN', 'Unknown');
 
-CREATE TABLE patient_order_outreach_result (
-  patient_order_outreach_result_id VARCHAR PRIMARY KEY,
-  patient_order_outreach_type_id VARCHAR REFERENCES patient_order_outreach_type,
+CREATE TABLE patient_order_outreach_result_type (
+  patient_order_outreach_result_type_id VARCHAR PRIMARY KEY,
   patient_order_outreach_result_status_id VARCHAR REFERENCES patient_order_outreach_result_status,
-  description VARCHAR NOT NULL,
+  description VARCHAR NOT NULL
+);
+
+INSERT INTO patient_order_outreach_result_type VALUES ('NO_ANSWER', 'NOT_CONNECTED', 'No Answer');
+INSERT INTO patient_order_outreach_result_type VALUES ('BUSY', 'NOT_CONNECTED', 'Busy');
+INSERT INTO patient_order_outreach_result_type VALUES ('LEFT_VOICEMAIL', 'NOT_CONNECTED', 'Left Voicemail');
+INSERT INTO patient_order_outreach_result_type VALUES ('LEFT_MESSAGE', 'NOT_CONNECTED', 'Left Message');
+INSERT INTO patient_order_outreach_result_type VALUES ('DISCONNECTED', 'NOT_CONNECTED', 'Disconnected');
+INSERT INTO patient_order_outreach_result_type VALUES ('WRONG_NUMBER', 'NOT_CONNECTED', 'Wrong Number');
+INSERT INTO patient_order_outreach_result_type VALUES ('DISCUSSED_APPOINTMENT', 'CONNECTED', 'Discussed Appointment');
+INSERT INTO patient_order_outreach_result_type VALUES ('DISCUSSED_DIGITAL_SCREENING_REMINDER', 'CONNECTED', 'Discussed Digital Screening Reminder');
+INSERT INTO patient_order_outreach_result_type VALUES ('DISCUSSED_RESOURCES', 'CONNECTED', 'Discussed Resources');
+INSERT INTO patient_order_outreach_result_type VALUES ('DISCUSSED_OTHER', 'CONNECTED', 'Discussed Other');
+INSERT INTO patient_order_outreach_result_type VALUES ('SENT_RESOURCES', 'CONNECTED', 'Sent Resources');
+INSERT INTO patient_order_outreach_result_type VALUES ('SENT_OTHER', 'CONNECTED', 'Sent Other');
+
+-- We can drive UI by grouping different types/statuses of outreach for user to select from
+CREATE TABLE patient_order_outreach_result (
+  patient_order_outreach_result_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  patient_order_outreach_type_id VARCHAR REFERENCES patient_order_outreach_type,
+  patient_order_outreach_result_type_id VARCHAR REFERENCES patient_order_outreach_result_type,
   display_order INTEGER NOT NULL
 );
 
 -- Phone Call
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('NO_ANSWER', 'PHONE_CALL', 'NOT_CONNECTED', 'No Answer', 1);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('BUSY', 'PHONE_CALL', 'NOT_CONNECTED', 'Busy', 2);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('LEFT_VOICEMAIL', 'PHONE_CALL', 'NOT_CONNECTED', 'Left Voicemail', 3);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('LEFT_MESSAGE', 'PHONE_CALL', 'NOT_CONNECTED', 'Left Message', 4);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('DISCONNECTED', 'PHONE_CALL', 'NOT_CONNECTED', 'Disconnected', 5);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('WRONG_NUMBER', 'PHONE_CALL', 'NOT_CONNECTED', 'Wrong Number', 6);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('DISCUSSED_APPOINTMENT', 'PHONE_CALL', 'CONNECTED', 'Discussed Appointment Time', 7);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('DISCUSSED_DIGITAL_SCREENING', 'PHONE_CALL', 'CONNECTED', 'Discussed Digital Screening Reminder', 8);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('DISCUSSED_RESOURCES', 'PHONE_CALL', 'CONNECTED', 'Discussed Resources', 9);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('DISCUSSED_OTHER', 'PHONE_CALL', 'CONNECTED', 'Discussed Other', 10);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'NO_ANSWER', 1);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'BUSY', 2);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'LEFT_VOICEMAIL', 3);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'LEFT_MESSAGE', 4);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'DISCONNECTED', 5);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'WRONG_NUMBER', 6);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'DISCUSSED_APPOINTMENT', 7);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'DISCUSSED_DIGITAL_SCREENING_REMINDER', 8);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'DISCUSSED_RESOURCES', 9);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('PHONE_CALL', 'DISCUSSED_OTHER', 10);
 
 -- MyChart
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('SENT_RESOURCES', 'MYCHART_MESSAGE', 'CONNECTED', 'Sent Resources', 11);
-INSERT INTO patient_order_outreach_result(patient_order_outreach_result_id, patient_order_outreach_type_id, patient_order_outreach_result_status_id, description, display_order)
-VALUES ('SENT_OTHER', 'MYCHART_MESSAGE', 'CONNECTED', 'Sent Other', 12);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('MYCHART_MESSAGE', 'SENT_RESOURCES', 11);
+INSERT INTO patient_order_outreach_result(patient_order_outreach_type_id, patient_order_outreach_result_type_id, display_order)
+VALUES ('MYCHART_MESSAGE', 'SENT_OTHER', 12);
 
-ALTER TABLE patient_order_outreach ADD COLUMN patient_order_outreach_type_id VARCHAR REFERENCES patient_order_outreach_type;
-ALTER TABLE patient_order_outreach ADD COLUMN patient_order_outreach_result_id VARCHAR REFERENCES patient_order_outreach_result;
+ALTER TABLE patient_order_outreach ADD COLUMN patient_order_outreach_result_id UUID REFERENCES patient_order_outreach_result;
 
--- This is nonproduction data currently; assign an arbitrary value
-UPDATE patient_order_outreach SET patient_order_outreach_type_id = 'PHONE_CALL';
-UPDATE patient_order_outreach SET patient_order_outreach_result_id = 'NO_ANSWER';
+-- This is nonproduction data currently; assign an arbitrary result
+UPDATE patient_order_outreach
+SET patient_order_outreach_result_id=porr.patient_order_outreach_result_id
+FROM patient_order_outreach_result porr
+WHERE porr.patient_order_outreach_type_id='PHONE_CALL'
+AND porr.patient_order_outreach_result_type_id='NO_ANSWER';
 
--- Doesn't really make sense to have a default value
-ALTER TABLE patient_order_outreach ALTER COLUMN patient_order_outreach_type_id SET NOT NULL;
 ALTER TABLE patient_order_outreach ALTER COLUMN patient_order_outreach_result_id SET NOT NULL;
 
 -- Different institutions can have different "branded" names for Epic MyChart
@@ -96,12 +115,15 @@ SELECT
   posm.patient_order_scheduled_message_id,
   posm.patient_order_id,
   posm.scheduled_message_id,
+  posm.patient_order_scheduled_message_type_id,
+  posmt.description AS patient_order_scheduled_message_type_description,
   posm.created,
   posm.last_updated,
   sm.scheduled_message_status_id,
   sm.scheduled_by_account_id,
   sm.scheduled_message_source_id,
   sm.message_type_id,
+  mt.description AS message_type_description,
   sm.scheduled_at,
   sm.time_zone,
   sm.processed_at,
@@ -109,9 +131,13 @@ SELECT
   sm.errored_at
 FROM
   patient_order_scheduled_message posm,
-  scheduled_message sm
+  patient_order_scheduled_message_type posmt,
+  scheduled_message sm,
+  message_type mt
 WHERE
-  posm.scheduled_message_id=sm.scheduled_message_id;
+  posm.scheduled_message_id=sm.scheduled_message_id
+  AND posm.patient_order_scheduled_message_type_id=posmt.patient_order_scheduled_message_type_id
+  AND sm.message_type_id=mt.message_type_id;
 
 -- TODO: finish up
 
