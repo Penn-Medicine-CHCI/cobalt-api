60fba478bafa382811b65e1a67c56dc0688acd2e
Mark Allen <mark.a.allen@gmail.com>
2023-05-22 14:07:32 -0400

    WIP for inapplicable screening questions

---
+++ sql/updates/093-ic-updates.sql
@@ -0,0 +1,34 @@
+BEGIN;
+SELECT _v.register_patch('093-ic-updates', NULL, NULL);
+
+-- As we progress through a specific screening during a screening session,
+-- we might need to mark questions as "inapplicable" - e.g. "If answered 'yes' to question 2, then skip questions 3 and 4 and go to question 5".
+--
+-- This is a different concept than screening_session_answered_screening_question, which is used
+-- to express that a question was actually answered, even if the "answer" was to skip or (for example) select zero options from a multiple choice
+CREATE TABLE screening_session_inapplicable_screening_question (
+  screening_session_inapplicable_screening_question_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	screening_session_screening_id UUID NOT NULL REFERENCES screening_session_screening,
+	screening_question_id UUID NOT NULL REFERENCES screening_question,
+	valid BOOLEAN NOT NULL DEFAULT TRUE, -- Similar to other screening tables, this "mark as inapplicable" can be invalidates if a user goes back and answers a question differently, potentially going down a different path/decision tree
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON screening_session_inapplicable_screening_question FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+CREATE VIEW v_screening_session_inapplicable_screening_question AS
+SELECT *
+FROM screening_session_inapplicable_screening_question
+WHERE valid=TRUE;
+
+-- Add Psychosis
+INSERT INTO screening_type (screening_type_id, description) VALUES ('PSYCHOSIS', 'Psychosis');
+
+-- Add C-SSRS-8.  Rename old C-SSRS to C-SSRS-3.
+INSERT INTO screening_type (screening_type_id, description) VALUES ('C_SSRS_8', 'C-SSRS-8');
+INSERT INTO screening_type (screening_type_id, description) VALUES ('C_SSRS_3', 'C-SSRS-3');
+UPDATE screening_type SET screening_type_id='C_SSRS_3' WHERE screening_type_id='C_SSRS';
+DELETE FROM screening_type WHERE screening_type_id='C_SSRS';
+
+COMMIT;
---
+++ src/main/java/com/cobaltplatform/api/model/db/ScreeningSessionInapplicableScreeningQuestion.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2021 The University of Pennsylvania and Penn Medicine
+ *
+ * Originally created at the University of Pennsylvania and Penn Medicine by:
+ * Dr. David Asch; Dr. Lisa Bellini; Dr. Cecilia Livesey; Kelley Kugler; and Dr. Matthew Press.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cobaltplatform.api.model.db;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * @author Transmogrify, LLC.
+ */
+@NotThreadSafe
+public class ScreeningSessionInapplicableScreeningQuestion {
+	@Nullable
+	private UUID screeningSessionInapplicableScreeningQuestionId;
+	@Nullable
+	private UUID screeningSessionScreeningId;
+	@Nullable
+	private UUID screeningQuestionId;
+	@Nullable
+	private Boolean valid;
+	@Nullable
+	private Instant created;
+	@Nullable
+	private Instant lastUpdated;
+
+	@Nullable
+	public UUID getScreeningSessionInapplicableScreeningQuestionId() {
+		return this.screeningSessionInapplicableScreeningQuestionId;
+	}
+
+	public void setScreeningSessionInapplicableScreeningQuestionId(@Nullable UUID screeningSessionInapplicableScreeningQuestionId) {
+		this.screeningSessionInapplicableScreeningQuestionId = screeningSessionInapplicableScreeningQuestionId;
+	}
+
+	@Nullable
+	public UUID getScreeningSessionScreeningId() {
+		return this.screeningSessionScreeningId;
+	}
+
+	public void setScreeningSessionScreeningId(@Nullable UUID screeningSessionScreeningId) {
+		this.screeningSessionScreeningId = screeningSessionScreeningId;
+	}
+
+	@Nullable
+	public UUID getScreeningQuestionId() {
+		return this.screeningQuestionId;
+	}
+
+	public void setScreeningQuestionId(@Nullable UUID screeningQuestionId) {
+		this.screeningQuestionId = screeningQuestionId;
+	}
+
+	@Nullable
+	public Boolean getValid() {
+		return this.valid;
+	}
+
+	public void setValid(@Nullable Boolean valid) {
+		this.valid = valid;
+	}
+
+	@Nullable
+	public Instant getCreated() {
+		return this.created;
+	}
+
+	public void setCreated(@Nullable Instant created) {
+		this.created = created;
+	}
+
+	@Nullable
+	public Instant getLastUpdated() {
+		return this.lastUpdated;
+	}
+
+	public void setLastUpdated(@Nullable Instant lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+}
