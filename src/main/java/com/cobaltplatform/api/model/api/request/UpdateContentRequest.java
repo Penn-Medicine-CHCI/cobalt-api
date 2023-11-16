c4ac993997736c3f571aa1a1f2aaeedcbfea59f8
Mark Allen <mark.a.allen@gmail.com>
2023-11-16 13:12:32 -0500

    Revert "Support for `contentStatusId` in content admin `PUT`"
    This reverts commit fadbc61708ad6e6977f0bb28a59d2e0bc48f7205.

--- src/main/java/com/cobaltplatform/api/model/api/request/UpdateContentRequest.java
+++ src/main/java/com/cobaltplatform/api/model/api/request/UpdateContentRequest.java
@@ -19,7 +19,7 @@
 
 package com.cobaltplatform.api.model.api.request;
 
-import com.cobaltplatform.api.model.db.ContentStatus.ContentStatusId;
+import com.cobaltplatform.api.model.db.ContentStatus;
 import com.cobaltplatform.api.model.db.ContentType.ContentTypeId;
 
 import javax.annotation.Nonnull;
@@ -38,8 +38,6 @@ public class UpdateContentRequest {
 	private UUID contentId;
 	@Nullable
 	private ContentTypeId contentTypeId;
-	@Nullable
-	private ContentStatusId contentStatusId;
 	@Nonnull
 	private String title;
 	@Nullable
@@ -66,7 +64,6 @@ public class UpdateContentRequest {
 	private Boolean sharedFlag;
 	@Nullable
 	private String fileUrl;
-
 	@Nullable
 	public UUID getContentId() {
 		return contentId;
@@ -83,15 +80,6 @@ public class UpdateContentRequest {
 
 	public void setContentTypeId(@Nullable ContentTypeId contentTypeId) {
 		this.contentTypeId = contentTypeId;
-	}
-
-	@Nullable
-	public ContentStatusId getContentStatusId() {
-		return this.contentStatusId;
-	}
-
-	public void setContentStatusId(@Nullable ContentStatusId contentStatusId) {
-		this.contentStatusId = contentStatusId;
 	}
 
 	@Nonnull
--- src/main/java/com/cobaltplatform/api/service/AdminContentService.java
+++ src/main/java/com/cobaltplatform/api/service/AdminContentService.java
@@ -307,7 +307,6 @@ public class AdminContentService {
 		String descriptionCommand = trimToNull(command.getDescription());
 		String authorCommand = trimToNull(command.getAuthor());
 		ContentTypeId contentTypeIdCommand = command.getContentTypeId();
-		ContentStatusId contentStatusId = command.getContentStatusId();
 		String durationInMinutesString = trimToNull(command.getDurationInMinutes());
 		Set<String> tagIds = command.getTagIds() == null ? Set.of() : command.getTagIds();
 		LocalDate publishStartDate = command.getPublishStartDate();
@@ -365,9 +364,6 @@ public class AdminContentService {
 
 			if (fileUrl != null)
 				existingContent.setFileUrl(fileUrl);
-
-			if (contentStatusId != null)
-				existingContent.setContentStatusId(contentStatusId);
 		}
 
 		if (durationInMinutesString != null && !ValidationUtility.isValidInteger(durationInMinutesString))
@@ -382,13 +378,13 @@ public class AdminContentService {
 		getDatabase().execute("""
 							 	UPDATE content SET content_type_id=?, title=?, url=?, image_url=?, 
 							 	duration_in_minutes=?, description=?, author=?, publish_start_date=?, publish_end_date=?,
-							 	publish_recurring=?, search_terms=?, shared_flag=?, file_url=?, content_status_id=?
+							 	publish_recurring=?, search_terms=?, shared_flag=?, file_url=?
 								WHERE content_id=?
 						""",
 				existingContent.getContentTypeId(), existingContent.getTitle(), existingContent.getUrl(), existingContent.getImageUrl(),
 				durationInMinutes, existingContent.getDescription(), existingContent.getAuthor(), existingContent.getPublishStartDate(), existingContent.getPublishEndDate(),
-				existingContent.getPublishRecurring(), existingContent.getSearchTerms(), existingContent.getSharedFlag(), existingContent.getFileUrl(),
-				existingContent.getContentStatusId(), existingContent.getContentId());
+				existingContent.getPublishRecurring(), existingContent.getSearchTerms(), existingContent.getSharedFlag(),existingContent.getFileUrl(),
+				existingContent.getContentId());
 
 		AdminContent adminContent = findAdminContentByIdForInstitution(account.getInstitutionId(), command.getContentId()).orElse(null);
 
@@ -515,7 +511,7 @@ public class AdminContentService {
 
 	@Nonnull
 	public void publishContent(@Nonnull UUID contentId,
-														 @Nonnull Account account) {
+																 @Nonnull Account account) {
 		requireNonNull(contentId);
 		requireNonNull(account);
 
@@ -535,7 +531,6 @@ public class AdminContentService {
 				WHERE content_id=?  				
 				""", contentId);
 	}
-
 	@Nonnull
 	public Optional<AdminContent> findAdminContentByIdForInstitution(@Nonnull InstitutionId institutionId, @Nonnull UUID contentId) {
 		requireNonNull(institutionId);
