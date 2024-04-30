dd200590aa7f1300394a5ee9f1129e0a3c353dcd
Mark Spence <mark@xmog.com>
2024-04-30 13:18:37 -0400

    Adding DB tracking

--- sql/updates/170-study-first-check-in-behavior.sql
+++ sql/updates/170-study-first-check-in-behavior.sql
@@ -1,4 +1,5 @@
 BEGIN;
+SELECT _v.register_patch('170-study-first-check-in-behavior', NULL, NULL);
 
 ALTER TABLE study ADD COLUMN leave_first_check_in_open_until_started BOOLEAN NOT NULL DEFAULT FALSE;
 
