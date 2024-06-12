1f35d586c9f48cc86c069d5e827c7e8edc821578
Mark Allen <mark.a.allen@gmail.com>
2024-06-12 10:23:30 -0400

    Data model for provider slot booking sync

---
+++ sql/updates/175-epic-provider-blocks.sql
@@ -0,0 +1,51 @@
+BEGIN;
+SELECT _v.register_patch('175-epic-provider-blocks', NULL, NULL);
+
+-- Set to true to enable Epic provider block syncing.
+ALTER TABLE institution ADD COLUMN epic_provider_slot_booking_sync_enabled BOOLEAN NOT NULL DEFAULT FALSE;
+
+-- Contact ID to key on, e.g. 'CSN'
+ALTER TABLE institution ADD COLUMN epic_provider_slot_booking_sync_contact_id_type TEXT;
+
+ -- Department ID to key on, e.g. 'INTERNAL'
+ALTER TABLE institution ADD COLUMN epic_provider_slot_booking_sync_department_id_type TEXT;
+
+ -- Visit type ID to key on, e.g. 'INTERNAL'
+ALTER TABLE institution ADD COLUMN epic_provider_slot_booking_sync_visit_type_id_type TEXT;
+
+-- We likely want to enforce scheduling limits for Epic appointments.
+-- For example, don't allow more than 1 NPV per schedule (morning or afternoon), where morning is 8-12 and afternoon is 1-5.
+CREATE TABLE epic_provider_schedule (
+	epic_provider_schedule_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	institution_id TEXT NOT NULL REFERENCES institution,
+	name TEXT NOT NULL,
+	start_time DATE NOT NULL,
+	end_time DATE NOT NULL,
+	maximum_npv_count INTEGER,
+	created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON epic_provider_schedule FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+CREATE UNIQUE INDEX epic_provider_schedule_name_unique_idx ON epic_provider_schedule USING btree (institution_id, LOWER(name));
+
+-- Synced-in booked slots from Epic for a given provider.
+-- This gives us sufficient information to enforce rules defined in epic_provider_schedule, e.g. "1 NPV max for the morning schedule"
+CREATE TABLE epic_provider_slot_booking (
+  epic_provider_slot_booking_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  provider_id UUID NOT NULL REFERENCES provider,
+  contact_id TEXT NOT NULL, -- Epic's 'appointment ID'
+  contact_id_type TEXT NOT NULL, -- e.g. 'CSN'
+  department_id TEXT NOT NULL, -- Appointment's Epic department ID
+  department_id_type TEXT NOT NULL, -- e.g. 'INTERNAL'
+  visit_type_id TEXT NOT NULL, -- Appointment's Epic visit type ID
+  visit_type_id_type TEXT NOT NULL, -- e.g. 'INTERNAL'
+  start_date_time TIMESTAMP WITHOUT TIME ZONE NOT NULL, -- Understood to be in provider's time zone
+  end_date_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,  -- Understood to be in provider's time zone
+  created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_last_updated BEFORE INSERT OR UPDATE ON epic_provider_slot_booking FOR EACH ROW EXECUTE PROCEDURE set_last_updated();
+
+COMMIT;
--- src/test/java/com/cobaltplatform/api/integration/epic/EpicIntegratedCareTests.java
+++ src/test/java/com/cobaltplatform/api/integration/epic/EpicIntegratedCareTests.java
@@ -19,12 +19,18 @@
 
 package com.cobaltplatform.api.integration.epic;
 
+import com.cobaltplatform.api.integration.epic.request.GetProviderAppointmentsRequest;
+import com.cobaltplatform.api.integration.epic.response.GetProviderAppointmentsResponse;
+import com.cobaltplatform.api.util.GsonUtility;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
 import javax.annotation.concurrent.ThreadSafe;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -32,6 +38,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 import static com.soklet.util.LoggingUtils.initializeLogback;
@@ -74,6 +82,47 @@ public class EpicIntegratedCareTests {
 		epicClient.encounterSearchFhirR4("eEMS.-CbIiYrvQbAjuUYABA3");
 	}
 
+	@Test
+	public void testProviderSlotBlocks() throws Exception {
+		EpicTestConfiguration epicTestConfiguration = epicTestConfigurationFromFile(Path.of("resources/test/epic-ic-prod-credentials.json"));
+		EpicConfiguration epicConfiguration = new EpicConfiguration.Builder(
+				epicTestConfiguration.getEpicEmpCredentials(),
+				epicTestConfiguration.getEpicEmpCredentials().getClientId(),
+				epicTestConfiguration.getBaseUrl()
+		).build();
+
+		EpicClient epicClient = new DefaultEpicClient(epicConfiguration);
+		ProviderForSlotBlocks providerForSlotBlocks = epicTestConfiguration.getExtraConfiguration().getProviderForSlotBlocks();
+
+		GetProviderAppointmentsRequest.Provider provider = new GetProviderAppointmentsRequest.Provider();
+		provider.setID(providerForSlotBlocks.getId());
+		provider.setIDType(providerForSlotBlocks.getIdType());
+		provider.setDepartmentID(providerForSlotBlocks.getDepartmentId());
+		provider.setDepartmentIDType(providerForSlotBlocks.getDepartmentIdType());
+
+		GetProviderAppointmentsRequest providerAppointmentsRequest = new GetProviderAppointmentsRequest();
+		providerAppointmentsRequest.setUserID(epicConfiguration.getEpicEmpCredentials().get().getUserId());
+		providerAppointmentsRequest.setUserIDType(epicConfiguration.getEpicEmpCredentials().get().getUserIdType());
+		providerAppointmentsRequest.setProviders(List.of(provider));
+		providerAppointmentsRequest.setStartDate(epicClient.formatDateWithSlashes(providerForSlotBlocks.getStartDate()));
+		providerAppointmentsRequest.setEndDate(epicClient.formatDateWithSlashes(providerForSlotBlocks.getEndDate()));
+
+		GetProviderAppointmentsResponse providerAppointmentsResponse = epicClient.getProviderAppointments(providerAppointmentsRequest);
+
+		// TODO: run tests
+
+		// 	@Nonnull
+		//	GetProviderAppointmentsResponse getProviderAppointments(@Nonnull GetProviderAppointmentsRequest request);
+		//
+		//	// epic/2012/Scheduling/Provider/GETPROVIDERAVAILABILITY/Availability
+		//	@Nonnull
+		//	GetProviderAvailabilityResponse getProviderAvailability(@Nonnull GetProviderAvailabilityRequest request);
+		//
+		//	// epic/2017/PatientAccess/External/GetScheduleDaysForProvider2/Scheduling/Open/Provider/GetScheduleDays2
+		//	@Nonnull
+		//	GetScheduleDaysForProviderResponse getScheduleDaysForProvider(@Nonnull GetScheduleDaysForProviderRequest request);
+	}
+
 	@Nonnull
 	protected EpicTestConfiguration epicTestConfigurationFromFile(@Nonnull Path file) {
 		requireNonNull(file);
@@ -81,22 +130,36 @@ public class EpicIntegratedCareTests {
 		if (!Files.exists(file))
 			throw new IllegalArgumentException(format("No such file %s", file.toAbsolutePath()));
 
+		GsonBuilder gsonBuilder = new GsonBuilder()
+				.setPrettyPrinting()
+				.disableHtmlEscaping();
+
+		GsonUtility.applyDefaultTypeAdapters(gsonBuilder);
+
+		Gson gson = gsonBuilder.create();
+
 		try {
 			String credentialsJson = Files.readString(file, StandardCharsets.UTF_8);
-			Map<String, String> credentials = new Gson().fromJson(credentialsJson, new TypeToken<Map<String, String>>() {
+			Map<String, Object> credentials = gson.fromJson(credentialsJson, new TypeToken<Map<String, Object>>() {
 				// Ignored
 			});
 
-			String clientId = credentials.get("clientId");
-			String userId = credentials.get("userId");
-			String userIdType = credentials.get("userIdType");
-			String username = credentials.get("username");
-			String password = credentials.get("password");
-			String baseUrl = credentials.get("baseUrl");
+			String clientId = (String) credentials.get("clientId");
+			String userId = (String) credentials.get("userId");
+			String userIdType = (String) credentials.get("userIdType");
+			String username = (String) credentials.get("username");
+			String password = (String) credentials.get("password");
+			String baseUrl = (String) credentials.get("baseUrl");
 
 			EpicEmpCredentials epicEmpCredentials = new EpicEmpCredentials(clientId, userId, userIdType, username, password);
 
-			return new EpicTestConfiguration(epicEmpCredentials, baseUrl);
+			Map<String, Object> rawExtraConfiguration = (Map<String, Object>) credentials.get("extraConfiguration");
+			ExtraConfiguration extraConfiguration = new ExtraConfiguration();
+
+			if (rawExtraConfiguration != null)
+				extraConfiguration = gson.fromJson(gson.toJson(rawExtraConfiguration), ExtraConfiguration.class);
+
+			return new EpicTestConfiguration(epicEmpCredentials, baseUrl, extraConfiguration);
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
@@ -108,14 +171,19 @@ public class EpicIntegratedCareTests {
 		private final EpicEmpCredentials epicEmpCredentials;
 		@Nonnull
 		private final String baseUrl;
+		@Nonnull
+		private final ExtraConfiguration extraConfiguration;
 
 		public EpicTestConfiguration(@Nonnull EpicEmpCredentials epicEmpCredentials,
-																 @Nonnull String baseUrl) {
+																 @Nonnull String baseUrl,
+																 @Nonnull ExtraConfiguration extraConfiguration) {
 			requireNonNull(epicEmpCredentials);
 			requireNonNull(baseUrl);
+			requireNonNull(extraConfiguration);
 
 			this.epicEmpCredentials = epicEmpCredentials;
 			this.baseUrl = baseUrl;
+			this.extraConfiguration = extraConfiguration;
 		}
 
 		@Nonnull
@@ -126,6 +194,107 @@ public class EpicIntegratedCareTests {
 		@Nonnull
 		public String getBaseUrl() {
 			return this.baseUrl;
+		}
+
+		@Nonnull
+		public ExtraConfiguration getExtraConfiguration() {
+			return this.extraConfiguration;
+		}
+	}
+
+	@NotThreadSafe
+	private static class ExtraConfiguration {
+		@Nullable
+		private ProviderForSlotBlocks providerForSlotBlocks;
+
+		@Nullable
+		public ProviderForSlotBlocks getProviderForSlotBlocks() {
+			return this.providerForSlotBlocks;
+		}
+
+		public void setProviderForSlotBlocks(@Nullable ProviderForSlotBlocks providerForSlotBlocks) {
+			this.providerForSlotBlocks = providerForSlotBlocks;
+		}
+	}
+
+	@NotThreadSafe
+	private static class ProviderForSlotBlocks {
+		@Nullable
+		private String name;
+		@Nullable
+		private String id;
+		@Nullable
+		private String idType;
+		@Nullable
+		private String departmentId;
+		@Nullable
+		private String departmentIdType;
+		@Nullable
+		private LocalDate startDate;
+		@Nullable
+		private LocalDate endDate;
+
+		@Nullable
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(@Nullable String name) {
+			this.name = name;
+		}
+
+		@Nullable
+		public String getId() {
+			return this.id;
+		}
+
+		public void setId(@Nullable String id) {
+			this.id = id;
+		}
+
+		@Nullable
+		public String getIdType() {
+			return this.idType;
+		}
+
+		public void setIdType(@Nullable String idType) {
+			this.idType = idType;
+		}
+
+		@Nullable
+		public String getDepartmentId() {
+			return this.departmentId;
+		}
+
+		public void setDepartmentId(@Nullable String departmentId) {
+			this.departmentId = departmentId;
+		}
+
+		@Nullable
+		public String getDepartmentIdType() {
+			return this.departmentIdType;
+		}
+
+		public void setDepartmentIdType(@Nullable String departmentIdType) {
+			this.departmentIdType = departmentIdType;
+		}
+
+		@Nullable
+		public LocalDate getStartDate() {
+			return this.startDate;
+		}
+
+		public void setStartDate(@Nullable LocalDate startDate) {
+			this.startDate = startDate;
+		}
+
+		@Nullable
+		public LocalDate getEndDate() {
+			return this.endDate;
+		}
+
+		public void setEndDate(@Nullable LocalDate endDate) {
+			this.endDate = endDate;
 		}
 	}
 }
