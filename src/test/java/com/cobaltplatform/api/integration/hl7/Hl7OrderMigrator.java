3e377b4025f39dffac64b55aa12d50627381c615
Mark Allen <mark.a.allen@gmail.com>
2024-07-12 14:37:26 -0400

    Adjust order migration to use UIDs as opposed to MRNs

--- src/test/java/com/cobaltplatform/api/integration/hl7/Hl7OrderMigrator.java
+++ src/test/java/com/cobaltplatform/api/integration/hl7/Hl7OrderMigrator.java
@@ -18,6 +18,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -43,14 +48,12 @@ public class Hl7OrderMigrator {
 
 		final String CURRENT_SYSTEM_ORDERS_FILE_ENV_VAR_NAME = "CURRENT_SYSTEM_ORDERS_FILE";
 		final String HL7_ORDERS_TO_MIGRATE_DIRECTORY_ENV_VAR_NAME = "HL7_ORDERS_TO_MIGRATE_DIRECTORY";
-		final String HL7_MRN_TYPE_ENV_VAR_NAME = "HL7_MRN_TYPE";
 		final String HL7_ORDERS_TO_MIGRATE_FILE_ENV_VAR_NAME = "HL7_ORDERS_TO_MIGRATE_FILE";
 
-		// e.g. \copy (select patient_order_id,order_date, patient_mrn, created at time zone 'America/New_York' as imported_at from patient_order where test_patient_order=false order by order_date) to 'imported-orders.csv' csv header;
+		// e.g. \copy (select patient_order_id,order_date, patient_unique_id, created at time zone 'America/New_York' as imported_at from patient_order where test_patient_order=false order by order_date) to 'imported-orders.csv' csv header;
 		String currentSystemOrdersFilename = extractEnvVarValue(CURRENT_SYSTEM_ORDERS_FILE_ENV_VAR_NAME);
 		String hl7OrdersToMigrateFilename = extractEnvVarValue(HL7_ORDERS_TO_MIGRATE_FILE_ENV_VAR_NAME);
 		String hl7OrdersToMigrateDirectoryName = extractEnvVarValue(HL7_ORDERS_TO_MIGRATE_DIRECTORY_ENV_VAR_NAME);
-		String hl7MrnType = extractEnvVarValue(HL7_MRN_TYPE_ENV_VAR_NAME);
 
 		Path currentSystemOrdersFile = Path.of(currentSystemOrdersFilename);
 
@@ -76,31 +79,29 @@ public class Hl7OrderMigrator {
 		if (!Files.isDirectory(hl7OrdersToMigrateDirectory))
 			throw new IllegalStateException(format("Path %s is not a directory", hl7OrdersToMigrateDirectory.toAbsolutePath()));
 
-		migrateHl7Orders(currentSystemOrdersFile, hl7OrdersToMigrateFile, hl7OrdersToMigrateDirectory, hl7MrnType);
+		migrateHl7Orders(currentSystemOrdersFile, hl7OrdersToMigrateFile, hl7OrdersToMigrateDirectory);
 	}
 
 	static void migrateHl7Orders(@Nonnull Path currentSystemOrdersFile,
 															 @Nonnull Path hl7OrdersToMigrateFile,
-															 @Nonnull Path hl7OrdersToMigrateDirectory,
-															 @Nonnull String hl7MrnType) throws IOException, Hl7ParsingException {
+															 @Nonnull Path hl7OrdersToMigrateDirectory) throws IOException, Hl7ParsingException {
 		requireNonNull(currentSystemOrdersFile);
 		requireNonNull(hl7OrdersToMigrateFile);
 		requireNonNull(hl7OrdersToMigrateDirectory);
-		requireNonNull(hl7MrnType);
 
-		Set<String> alreadyImportedMrns = new LinkedHashSet<>();
+		Set<String> alreadyImportedUids = new LinkedHashSet<>();
 
 		// Pull order data from the existing IC instance's database export
 		try (Reader reader = new FileReader(currentSystemOrdersFile.toFile(), StandardCharsets.UTF_8)) {
 			for (CSVRecord record : CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader)) {
-				String mrn = trimToNull(record.get(2));
-				alreadyImportedMrns.add(mrn);
+				String uid = trimToNull(record.get(2));
+				alreadyImportedUids.add(uid);
 			}
 		}
 
-		System.out.println(format("Already imported %d MRNs", alreadyImportedMrns.size()));
+		System.out.println(format("Already imported %d UIDs", alreadyImportedUids.size()));
 
-		Set<String> mrnsToMigrate = new LinkedHashSet<>();
+		Set<String> uidsToMigrate = new LinkedHashSet<>();
 
 		// Pull data from the "to migrate" CSV
 		try (Reader reader = new FileReader(hl7OrdersToMigrateFile.toFile(), StandardCharsets.UTF_8)) {
@@ -108,24 +109,21 @@ public class Hl7OrderMigrator {
 				// String practice = StringUtils.trimToNull(record.get(0));
 				// String firstName = StringUtils.trimToNull(record.get(1));
 				// String lastName = StringUtils.trimToNull(record.get(2));
-				String mrnToMigrate = trimToNull(record.get(3));
+				String uidToMigrate = trimToNull(record.get(3));
 				// String orderDateAsString = StringUtils.trimToNull(record.get(4));
 
-				// MRN has a leading value that needs to be removed
-				mrnToMigrate = mrnToMigrate.substring(1);
+				if (uidsToMigrate.contains(uidToMigrate))
+					throw new IllegalStateException(format("Duplicate UID encountered: %s", uidToMigrate));
 
-				if (mrnsToMigrate.contains(mrnToMigrate))
-					throw new IllegalStateException(format("Duplicate MRN encountered: %s", mrnToMigrate));
-
-				mrnsToMigrate.add(mrnToMigrate);
+				uidsToMigrate.add(uidToMigrate);
 			}
 		}
 
-		System.out.printf("MRNs to migrate: %d\n", mrnsToMigrate.size());
+		System.out.printf("UIDs to migrate: %d\n", uidsToMigrate.size());
 
-		Set<String> mrnsToMigrateThatWereAlreadyImported = Sets.intersection(alreadyImportedMrns, mrnsToMigrate);
+		Set<String> uidsToMigrateThatWereAlreadyImported = Sets.intersection(alreadyImportedUids, uidsToMigrate);
 
-		System.out.printf("MRNs to migrate that were already imported: %s\n", mrnsToMigrateThatWereAlreadyImported);
+		System.out.printf("UIDs to migrate that were already imported: %s\n", uidsToMigrateThatWereAlreadyImported);
 
 		List<Path> hl7OrderFiles = Stream.of(hl7OrdersToMigrateDirectory.toFile().listFiles())
 				.filter(file -> !file.isDirectory() && file.getName().startsWith("To_COBALT_Ord_"))
@@ -135,7 +133,8 @@ public class Hl7OrderMigrator {
 
 		System.out.printf("Processing %d order file[s]...\n", hl7OrderFiles.size());
 
-		Map<String, Path> hl7OrderFilesToMigrateByMrn = new LinkedHashMap<>();
+		Map<String, Path> hl7OrderFilesToMigrateByUid = new LinkedHashMap<>();
+		Set<String> uidsWithExistingOrdersOnFile = new HashSet<>();
 
 		Hl7Client hl7Client = new Hl7Client();
 
@@ -150,36 +149,50 @@ public class Hl7OrderMigrator {
 			Hl7OrderSection order = generalOrder.getOrders().get(0);
 			Hl7PatientSection patient = generalOrder.getPatient();
 
-			Hl7ExtendedCompositeIdWithCheckDigit patientMrn = patient.getPatientIdentification().getPatientIdentifierList().stream()
+			Hl7ExtendedCompositeIdWithCheckDigit patientUniqueIdentifier = patient.getPatientIdentification().getPatientIdentifierList().stream()
 					.filter(patientIdentifier -> {
-						return Objects.equals(hl7MrnType, patientIdentifier.getIdentifierTypeCode());
+						return Objects.equals("UID", patientIdentifier.getIdentifierTypeCode());
 					})
 					.findFirst()
 					.orElse(null);
 
-			if (patientMrn == null)
-				throw new IllegalStateException(format("Unable to find patient MRN in %s", hl7OrderFile.toAbsolutePath()));
+			if (patientUniqueIdentifier == null)
+				throw new IllegalStateException(format("Unable to find patient UID in %s", hl7OrderFile.toAbsolutePath()));
 
-			String orderFileMrn = trimToNull(patientMrn.getIdNumber());
+			String orderFileUid = trimToNull(patientUniqueIdentifier.getIdNumber());
 
-			if (mrnsToMigrate.contains(orderFileMrn)) {
-				if (hl7OrderFilesToMigrateByMrn.containsKey(orderFileMrn))
-					System.out.printf("Already have an order file for MRN %s\n", orderFileMrn);
+			if (uidsToMigrate.contains(orderFileUid)) {
+				if (hl7OrderFilesToMigrateByUid.containsKey(orderFileUid)) {
+					if(uidsWithExistingOrdersOnFile.contains(orderFileUid)) {
+						// Don't show the same UID twice to avoid duplicate debugging output lines
+					} else {
+						Path existingHl7OrderFile = hl7OrderFilesToMigrateByUid.get(orderFileUid);
+						byte[] existingGeneralOrderHl7 = Files.readAllBytes(existingHl7OrderFile);
+						String existingGeneralOrderHl7AsString = hl7Client.messageFromBytes(existingGeneralOrderHl7);
+						Hl7GeneralOrderTriggerEvent existingGeneralOrder = hl7Client.parseGeneralOrder(existingGeneralOrderHl7AsString);
 
-				hl7OrderFilesToMigrateByMrn.put(orderFileMrn, hl7OrderFile);
+						Instant orderDate = order.getCommonOrder().getDateTimeOfTransaction().getTime();
+						LocalDateTime orderDateAsLocalDateTime = LocalDateTime.ofInstant(orderDate, ZoneId.of("America/New_York"));
+
+						System.out.printf("Already have an order on file for UID %s. Order date was %s\n", orderFileUid, orderDateAsLocalDateTime);
+						uidsWithExistingOrdersOnFile.add(orderFileUid);
+					}
+				}
+
+				hl7OrderFilesToMigrateByUid.put(orderFileUid, hl7OrderFile);
 			}
 		}
 
-		for (Entry<String, Path> entry : hl7OrderFilesToMigrateByMrn.entrySet()) {
-			String mrn = entry.getKey();
+		for (Entry<String, Path> entry : hl7OrderFilesToMigrateByUid.entrySet()) {
+			String uid = entry.getKey();
 			Path hl7OrderFile = entry.getValue();
 
-			System.out.printf("%s=%s\n", mrn, hl7OrderFile);
+			System.out.printf("%s=%s\n", uid, hl7OrderFile);
 		}
 
-		Set<String> mrnsToMigrateWithoutHl7OrderFiles = Sets.difference(mrnsToMigrate, hl7OrderFilesToMigrateByMrn.keySet());
+		Set<String> uidsToMigrateWithoutHl7OrderFiles = Sets.difference(uidsToMigrate, hl7OrderFilesToMigrateByUid.keySet());
 
-		System.out.printf("There are %d MRNs that do not have corresponding HL7 order files.\n", mrnsToMigrateWithoutHl7OrderFiles.size());
+		System.out.printf("There are %d UIDs that do not have corresponding HL7 order files: %s\n", uidsToMigrateWithoutHl7OrderFiles.size(), uidsToMigrateWithoutHl7OrderFiles);
 	}
 
 	@Nonnull
