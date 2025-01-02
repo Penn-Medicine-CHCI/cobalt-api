042644428cd0a28b7034500e98dbd6c6f6a311d8
Mark Spence <mark@xmog.com>
2025-01-02 13:58:28 -0500

    Updating to postgis/postgis:16-3.5 image

--- sql/initial/recreate.sql
+++ sql/initial/recreate.sql
@@ -11,6 +11,21 @@ WHERE
     AND datname = 'cobalt'
 ;
 
+--Revoke all of the Foreign Data Wrapper permissions so the cobalt role can be dropped
+REVOKE ALL ON FUNCTION postgres_fdw_handler() FROM cobalt;
+REVOKE ALL ON FUNCTION postgres_fdw_validator(text[], oid) FROM cobalt;
+REVOKE ALL ON FUNCTION postgres_fdw_get_connections() FROM cobalt;
+REVOKE ALL ON FUNCTION postgres_fdw_disconnect(text) FROM cobalt;
+REVOKE ALL ON FUNCTION postgres_fdw_disconnect_all() FROM cobalt;
+
+--Reubild the template database. This is really only needed if we're swiching BD images/versions
+UPDATE pg_database SET datallowconn = 'false' WHERE datname = 'template1';
+ALTER DATABASE template1 IS_TEMPLATE = false;
+DROP DATABASE template1;
+CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+UPDATE pg_database SET datistemplate = true WHERE datname = 'template1';
+ALTER DATABASE template1 ALLOW_CONNECTIONS = true;
+
 -- DROP SCHEMA IF EXISTS ic CASCADE;
 DROP DATABASE cobalt;
 DROP ROLE cobalt;
@@ -36,6 +51,7 @@ CREATE EXTENSION hstore SCHEMA cobalt;
 CREATE EXTENSION pg_stat_statements WITH SCHEMA cobalt;
 --CREATE EXTENSION "plv8";
 CREATE EXTENSION postgres_fdw;
+CREATE EXTENSION postgis WITH SCHEMA cobalt;
 GRANT USAGE ON FOREIGN DATA WRAPPER postgres_fdw TO cobalt; 
 
 CREATE USER fdwuser WITH PASSWORD 'password';
